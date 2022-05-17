24dbf66510c3700e524a953e0121c6f5b9e18b28
alexjhbarber <48556277+alexjhbarber@users.noreply.github.com>
2022-05-17 21:40:54 +0100

    created migration for subjects table and removed un-needed migrations

--- database/migrations/2022_05_17_203451_subjects.php
+++ database/migrations/2022_05_17_203451_subjects.php
@@ -13,15 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        //
+        Schema::create('subjects', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('name');
+            $table->string('cohort');
+            $table->timestamp('date_of_birth');
+            
         });
+    
     }
 
     /**
@@ -31,6 +31,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        //
+        Schema::drop('subjects');
     }
 };
